76eeee01112a1680734367889e695cc43d0fe1d3
Christoph Engelbert (noctarius) <me@noctarius.com>
2025-04-17 21:34:19 +0200

    Added initial file creation through /dev/urandom

--- fio/fio.py
+++ fio/fio.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import re
 import sys
 from pathlib import Path
 import configparser
@@ -24,6 +25,63 @@ class Benchmark:
         self.blocksize = blocksize
         self.workload_name = workload_name
         self.workload_file = workload_file
+
+
+def _parse_unit(unit: str, mode: str = 'si/iec', strict: bool = True) -> tuple[int, int]:
+    """Parse the given unit, returning the associated base and exponent
+
+    Mode can be either 'si/iec' to parse decimal (SI) and binary (IEC) units, or
+    'jedec' for binary only units. If `strict`, parsing will be case-sensitive and
+    expect the 'B' suffix.
+    """
+    regexes = {
+        'si/iec': r'^((?P<prefix>[kKMGTPEZ])(?P<binary>i)?)?' + ('B$' if strict else 'B?$'),
+        'jedec': r'^(?P<prefix>[KMGTPEZ])?' + ('B$' if strict else 'B?$'),
+    }
+
+    m = re.match(regexes[mode], unit, flags=re.IGNORECASE if not strict else 0)
+    if m is None:
+        raise ValueError("Invalid unit")
+
+    binary = (mode == 'jedec') or (m.group('binary') is not None)
+    prefix = m.group('prefix') or ''
+
+    if strict and (binary and (prefix == 'k')) or ((not binary) and (prefix == 'K')):
+        raise ValueError("Invalid unit")
+
+    exponent_multipliers = ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']
+    return (
+        2 if binary else 10,
+        (10 if binary else 3) * exponent_multipliers.index(prefix.upper())
+    )
+
+
+def parse_size(size_string: str, mode: str = 'si/iec', unit: str = '', strict: bool = False) -> int:
+    """Parse the given data size
+
+    If passed and not explicitly given, 'unit' will be assumed.
+    Mode can be either 'si/iec' to parse decimal (SI) and binary (IEC) units, or
+    'jedec' for binary only units. If `strict`, parsing will be case-sensitive and
+    expect the 'B' suffix.
+    """
+    if not unit:
+        try:
+            x = int(size_string)
+            return x
+        except Exception:
+            pass
+
+    try:
+        m = re.match(r'^(?P<size_in_unit>\d+) ?(?P<unit>\w+)?$', size_string.strip())
+        if m is None:
+            raise ValueError(f"Invalid size: {size_string}")
+
+        size_in_unit = int(m.group('size_in_unit'))
+        unit = m.group('unit') if m.group('unit') else unit
+        base, exponent = _parse_unit(unit, mode, strict=strict)
+        return size_in_unit * (base ** exponent)
+    except ValueError:
+        return -1
 
 
 def list_available_workloads():
@@ -101,12 +159,31 @@ def run_single_workload(worker_dir: str, loops: int, benchmark: Benchmark):
     run_fio(worker_dir, workload_name, loops, benchmark.io_depth, benchmark.worker, benchmark.blocksize)
 
 
+def create_data_file(id: int, file_size: int):
+    count = file_size / 1024**2
+    working_directory = os.getcwd()
+    data_file = os.path.join(working_directory, f"fio-benchmark-data-{id}")
+    subprocess.check_output(
+        ["dd", "if=/dev/urandom", f"of={data_file}", "bs=1M", f"count={count}"],
+        stderr=subprocess.STDOUT,
+    )
+
+
 def run_workloads(worker_dir: str, loops: int, benchmarks: list[Benchmark]):
+    file_size = parse_size(get_data_file_size())
+    print("Laying out data files...", end='', flush=True)
+    worker = benchmarks[0].worker
+    for i in range(0, worker):
+        print(f"{i + 1}", end='', flush=True)
+        create_data_file(i, file_size)
+        print("...", end='', flush=True)
+    print(" done.")
+
     for benchmark in benchmarks:
         run_single_workload(worker_dir, loops, benchmark)
 
 
-def estimated_time_per_benchmark():
+def estimated_time_per_benchmark() -> int:
     config = configparser.ConfigParser(allow_no_value=True)
     filepath = os.path.join(script_path, "workloads/global")
     config.read_string("[global]\n" + Path(filepath).read_text())
@@ -114,6 +191,14 @@ def estimated_time_per_benchmark():
     runtime = config.getint("global", "runtime") if config.has_option("global", "runtime") else 0
     rampup = config.getint("global", "ramp_time") if config.has_option("global", "ramp_time") else 0
     return rampup + runtime
+
+
+def get_data_file_size() -> str:
+    config = configparser.ConfigParser(allow_no_value=True)
+    filepath = os.path.join(script_path, "workloads/global")
+    config.read_string("[global]\n" + Path(filepath).read_text())
+
+    return config.getint("global", "size") if config.has_option("global", "size") else "1G"
 
 
 def run_all_workloads():
