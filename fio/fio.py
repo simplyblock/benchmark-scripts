c65855a466be74a8cf2c2484776d98184de1ae9f
Christoph Engelbert (noctarius) <me@noctarius.com>
2025-04-24 16:04:55 +0200

    Added command to summarize the logs into a csv

--- fio/fio.py
+++ fio/fio.py
@@ -4,6 +4,7 @@
 import argparse
 import os
 from fiobench.benchmark import Benchmark
+from fiobench.analyze import Analyzer
 
 
 script_path = os.path.dirname(os.path.realpath(__file__))
@@ -18,9 +19,12 @@ if __name__ == "__main__":
     command_parser = parser.add_subparsers(dest="command")
 
     benchmark_parser = command_parser.add_parser("run", help="Run a benchmark definition")
-    benchmark_parser.add_argument("--benchmark-file", required=True, type=str)
-    benchmark_parser.add_argument("--worker-dir", required=True, type=str)
-    benchmark_parser.add_argument("--workload-dir", required=False, type=str)
+    benchmark_parser.add_argument("--benchmark-file", "-b", required=True, type=str, dest="benchmark_file")
+    benchmark_parser.add_argument("--worker-dir", "-w", required=True, type=str, dest="worker_dir")
+    benchmark_parser.add_argument("--workload-dir" "-wl", required=False, type=str, dest="workload_dir")
+
+    analytics_parser = command_parser.add_parser("analyze", help="Analyze benchmark results")
+    analytics_parser.add_argument("--benchmark-result-dir", "-b", required=True, type=str, dest="benchmark_result_dir")
 
     namespace = parser.parse_args()
 
@@ -30,5 +34,9 @@ if __name__ == "__main__":
         workload_dir = namespace.workload_dir if namespace.workload_dir else script_path
         benchmark = Benchmark(benchmark_file, worker_dir, workload_dir)
         benchmark.run_all_workloads()
+    elif namespace.command == "analyze":
+        benchmark_result_dir = namespace.benchmark_result_dir
+        analyzer = Analyzer(benchmark_result_dir)
+        analyzer.analyze()
     else:
         parser.print_help()
--- fio/fiobench/analyze.py
+++ fio/fiobench/analyze.py
@@ -3,60 +3,73 @@ import sys
 import os
 from pathlib import Path
 
-benchmark_loop_files = {}
-for dirpath, dirnames, filenames in os.walk(sys.argv[1]):
-    for filename in filenames:
-        groupby = filename[:len(filename)-6]
 
-        if not groupby in benchmark_loop_files:
-            benchmark_loop_files[groupby] = []
+class Analyzer:
+    def __init__(self, benchmark_result_dir: str):
+        self.benchmark_result_dir = benchmark_result_dir
 
-        files = benchmark_loop_files[groupby]
-        path = os.path.join(dirpath, filename)
+    def analyze(self):
+        benchmark_loop_files = {}
+        for dirpath, dirnames, filenames in os.walk(self.benchmark_result_dir):
+            for filename in filenames:
+                if not filename.endswith(".json"):
+                    continue
 
-        files.append(path)
+                groupby = filename[:len(filename) - 6]
+                if not groupby in benchmark_loop_files:
+                    benchmark_loop_files[groupby] = []
 
-print ('name,blocksize,iodepth,workers,read_iops,write_iops,total_iops,avg_read_throughput,avg_write_throughput')
-for benchmark_group in benchmark_loop_files.values():
-    read_iops=[]
-    write_iops=[]
-    read_throughputs = []
-    write_throughputs = []
+                files = benchmark_loop_files[groupby]
+                path = os.path.join(dirpath, filename)
 
-    num_of_loops = -1
-    benchmark_name = ""
-    num_jobs = 0
-    blocksize = "0K"
-    iodepth = 0
-    for file in benchmark_group:
-        content = json.loads(Path(file).read_text())
+                files.append(path)
 
-        benchmark_name = content['jobs'][1]['jobname']
-        num_jobs = content['global options']['numjobs']
-        blocksize = content['global options']['bs']
-        iodepth = content['global options']['iodepth']
+        with open(os.path.join(self.benchmark_result_dir, "summary.csv"), "w") as f:
+            f.write(
+                "name,blocksize,iodepth,workers,read_iops,write_iops,total_iops,avg_read_throughput,avg_write_throughput\n")
+            for benchmark_group in benchmark_loop_files.values():
+                read_iops = []
+                write_iops = []
+                read_throughputs = []
+                write_throughputs = []
 
-        num_of_loops = len(content['jobs'])
+                num_of_loops = -1
+                benchmark_name = ""
+                num_jobs = 0
+                blocksize = "0K"
+                iodepth = 0
+                for file in benchmark_group:
+                    content = json.loads(Path(file).read_text())
 
-        job_read_iops = []
-        job_write_iops = []
-        job_read_throughputs = []
-        job_write_throughputs = []
-        for job in content['jobs']:
-            job_read_iops.append(job['read']['iops_mean'])
-            job_write_iops.append(job['write']['iops_mean'])
-            job_read_throughputs.append(job['read']['bw_bytes'])
-            job_write_throughputs.append(job['write']['bw_bytes'])
+                    benchmark_name = content['jobs'][1]['jobname']
+                    num_jobs = content['global options']['numjobs']
+                    blocksize = content['global options']['bs']
+                    iodepth = content['global options']['iodepth']
 
-        read_iops.append(sum(job_read_iops))
-        write_iops.append(sum(job_write_iops))
-        read_throughputs.append(sum(job_read_throughputs))
-        write_throughputs.append(sum(job_write_throughputs))
+                    num_of_loops = len(content['jobs'])
 
-    avg_read_iops = round(sum(read_iops) / len(read_iops), 2)
-    avg_write_iops = round(sum(write_iops) / len(write_iops), 2)
-    avg_total_iops = round(avg_write_iops + avg_read_iops, 2)
-    avg_read_throughput = round(sum(read_throughputs) / len(read_throughputs) / 1000 / 1000 * 8, 2)
-    avg_write_throughput = round(sum(write_throughputs) / len(write_throughputs) / 1000 / 1000 * 8, 2)
+                    job_read_iops = []
+                    job_write_iops = []
+                    job_read_throughputs = []
+                    job_write_throughputs = []
+                    for job in content['jobs']:
+                        job_read_iops.append(job['read']['iops_mean'])
+                        job_write_iops.append(job['write']['iops_mean'])
+                        job_read_throughputs.append(job['read']['bw_bytes'])
+                        job_write_throughputs.append(job['write']['bw_bytes'])
 
-    print(f"{benchmark_name},{blocksize},{iodepth},{num_jobs},{avg_read_iops},{avg_write_iops},{avg_total_iops},{avg_read_throughput},{avg_write_throughput}")
+                    read_iops.append(sum(job_read_iops))
+                    write_iops.append(sum(job_write_iops))
+                    read_throughputs.append(sum(job_read_throughputs))
+                    write_throughputs.append(sum(job_write_throughputs))
+
+                avg_read_iops = round(sum(read_iops) / len(read_iops), 2)
+                avg_write_iops = round(sum(write_iops) / len(write_iops), 2)
+                avg_total_iops = round(avg_write_iops + avg_read_iops, 2)
+                avg_read_throughput = round(sum(read_throughputs) / len(read_throughputs) / 1000 / 1000 * 8, 2)
+                avg_write_throughput = round(sum(write_throughputs) / len(write_throughputs) / 1000 / 1000 * 8, 2)
+
+                f.write(
+                    f"{benchmark_name},{blocksize},{iodepth},{num_jobs},{avg_read_iops},{avg_write_iops},"
+                    f"{avg_total_iops},{avg_read_throughput},{avg_write_throughput}\n"
+                )
